fa4746841ac72bfc4cbce0bd70d2113ed620b780
Sean Holcomb <seaholcomb@gmail.com>
2019-03-03 15:27:14 -0500

    Refactoring frequency generation (wip)

--- src/waveslab.js
+++ src/waveslab.js
@@ -8,9 +8,14 @@
 //==============================================================================
 function Waveform({
   container,
-  barSize = 5, spacing = 1, amplitude= 0.25, cursorSize = 2,
-  mainColor = "#ffffff", progressionColor = "#869aba", cursorColor = "#ffffff"
-}){
+  barSize = 5,
+  spacing = 1,
+  amplitude = 0.25,
+  cursorSize = 2,
+  mainColor = "#ffffff",
+  progressionColor = "#869aba",
+  cursorColor = "#ffffff"
+}) {
 
   //get container
   var container = document.getElementById(container);
@@ -27,7 +32,7 @@ function Waveform({
   const HEIGHT = this.canvas.height;
 
   //setup audio context
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
 
   //capture arguments
   var barSize = barSize;
@@ -48,32 +53,34 @@ function Waveform({
   //genWaveform(): Loads audio and launches animation
   //               once audio is ready
   //---------------------------------------------------------
-  this.genWaveform = function(url){
-    document.addEventListener('audioReady', () => { this.render(); }, false);
+  this.genWaveform = function(url) {
+    document.addEventListener('audioReady', () => {
+      this.render();
+    }, false);
     this.loadAudio(url);
   }
 
   //---------------------------------------------------------
   //playPause(): alternates between playing and pausing playback
   //---------------------------------------------------------
-  this.playPause = function(){
-    if(!playing){
-  		source = audioCtx.createBufferSource();
-  		source.buffer = this.audBuffer;
-  		source.connect(audioCtx.destination);
-  		source.start(audioCtx.currentTime, playbackTime);
-  		pastTime = audioCtx.currentTime;
-  		playing = true;
-  	}
-  	else{
-  		source.stop();
-  		playing = false;
-  	}
+  this.playPause = function() {
+    if (!playing) {
+      source = audioCtx.createBufferSource();
+      source.buffer = this.audBuffer;
+      source.connect(audioCtx.destination);
+      source.start(audioCtx.currentTime, playbackTime);
+      pastTime = audioCtx.currentTime;
+      playing = true;
+    }
+    else {
+      source.stop();
+      playing = false;
+    }
   }
   //---------------------------------------------------------
   //loadAudio(): loads the audio and stores results
   //---------------------------------------------------------
-  this.loadAudio = function(url){
+  this.loadAudio = function(url) {
     var readyEvent = new Event('audioReady');
     var self = this;
     var request = new XMLHttpRequest();
@@ -83,14 +90,13 @@ function Waveform({
     // Decode asynchronously
     request.onload = function() {
       audioCtx.decodeAudioData(request.response,
-  			function(buffer){
-  				self.audBuffer = buffer;
-  				self.duration = buffer.duration;
+        function(buffer) {
+          self.audBuffer = buffer;
+          self.duration = buffer.duration;
           self.chanData = buffer.getChannelData(0);
           self.step = Math.ceil(self.chanData.length / WIDTH);
           document.dispatchEvent(readyEvent);
-          console.log("okay to draw!");
-  			});
+        });
     }
     request.send();
   }
@@ -101,37 +107,37 @@ function Waveform({
   this.draw = function() {
     canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-  	for(var i = 0; i < WIDTH; i += spacing){
-  		var min = 1.0;
-  		var max = -1.0;
-  		for(var j = 0; j < this.step; j++){
-  			var val = this.chanData[(i*this.step)+j];
-  			if(val < min)
-  				min = val;
-  			if(val > max)
-  				max = val;
-  		}
+    for (var i = 0; i < WIDTH; i += spacing) {
+      var min = 1.0;
+      var max = -1.0;
+      for (var j = 0; j < this.step; j++) {
+        var val = this.chanData[(i * this.step) + j];
+        if (val < min)
+          min = val;
+        if (val > max)
+          max = val;
+      }
 
       //set color based on progression
-  		if(i / WIDTH * this.duration <= playbackTime)
-  			canvasCtx.fillStyle = progressionColor;
-  		else
-  			canvasCtx.fillStyle = mainColor;
+      if (i / WIDTH * this.duration <= playbackTime)
+        canvasCtx.fillStyle = progressionColor;
+      else
+        canvasCtx.fillStyle = mainColor;
 
       //draw the data bar
-  		canvasCtx.fillRect(i, HEIGHT/2 + (min)*amplitude, barSize, Math.max(1,(max-min)*amplitude));
-  	}
+      canvasCtx.fillRect(i, HEIGHT / 2 + (min) * amplitude, barSize, Math.max(1, (max - min) * amplitude));
+    }
 
-  	//draw progression bar
-  	canvasCtx.fillStyle = cursorColor;
-  	canvasCtx.fillRect(playbackTime/this.duration * WIDTH, HEIGHT/2 - amplitude, cursorSize, amplitude*2 );
+    //draw progression bar
+    canvasCtx.fillStyle = cursorColor;
+    canvasCtx.fillRect(playbackTime / this.duration * WIDTH, HEIGHT / 2 - amplitude, cursorSize, amplitude * 2);
   }
 
   //---------------------------------------------------------
   //setPlayback(): Handles progression of playback during render
   //---------------------------------------------------------
-  function syncTime(){
-    if(playing){
+  function syncTime() {
+    if (playing) {
       playbackTime = playbackTime + audioCtx.currentTime - pastTime;
       pastTime = audioCtx.currentTime;
     }
@@ -139,45 +145,60 @@ function Waveform({
   //---------------------------------------------------------
   //handleWaveClick(): handles when waveform is clicked with mouse
   //---------------------------------------------------------
-  function handleWaveClick(){
-    var x  = event.clientX - this.canvas.offsetLeft;
-  	playbackTime = (x/WIDTH * this.duration);
+  function handleWaveClick() {
+    var x = event.clientX - this.canvas.offsetLeft;
+    playbackTime = (x / WIDTH * this.duration);
     pastTime = audioCtx.currentTime;
-    if(playing){
-    	source.stop();
-    	source = audioCtx.createBufferSource();
-    	source.buffer = this.audBuffer;
-    	source.connect(audioCtx.destination);
-    	source.start(audioCtx.currentTime, playbackTime);
+    if (playing) {
+      source.stop();
+      source = audioCtx.createBufferSource();
+      source.buffer = this.audBuffer;
+      source.connect(audioCtx.destination);
+      source.start(audioCtx.currentTime, playbackTime);
 
-    	playing = true;
+      playing = true;
     }
   }
   //---------------------------------------------------------
   //render(): render loop for animating and timing audio waveform
   //---------------------------------------------------------
-  this.render = function(){
+  this.render = function() {
 
     this.draw();
     syncTime();
-    requestAnimationFrame(() => {this.render();});
+    requestAnimationFrame(() => {
+      this.render();
+    });
   }
 } //end Waveform
 
 //==============================================================================
-//FrequencyChart: uses analyser node to create bar chart of current playing audio
+//HtmlFrequencyChart: uses analyser node to create bar chart of current playing audio
 //==============================================================================
-function FrequencyChart({
+function HtmlFrequencyChart({
   container,
-  barSize = 5, amplitude= 0.33, fftSize = 8, spacing = 2,
+  url,
+  barSize = 5,
+  amplitude = 0.33,
+  fftSize = 8,
+  spacing = 2,
   bottomAmplitude = amplitude,
-  mainColor = "#42cef4", bottomColor = mainColor
-}){
+  mainColor = "#42cef4",
+  bottomColor = mainColor,
+  useAudioControls = true
+}) {
 
-  //get container
+  var audioCtx;
+  var analyser;
+  var fftSize = fftSize;
+  var source, bufferLength, dataArray, barWidth, barHeight;
+  var mainColor = mainColor;
+  var isGenerated = false;
+
+  // Get container for the canvas.
   var container = document.getElementById(container);
 
-  //setup canvas. Keeping accessible for user manipulation
+  // Setup canvas. Keeping accessible for user manipulation.
   this.canvas = document.createElement('canvas');
   this.canvas.width = container.offsetWidth;
   this.canvas.height = container.offsetHeight;
@@ -187,25 +208,24 @@ function FrequencyChart({
   const WIDTH = this.canvas.width;
   const HEIGHT = this.canvas.height;
 
-  //audio element
-  var audio = new Audio();
-
-  //audio context
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  var analyser = audioCtx.createAnalyser();
-
-  var fftSize = Math.pow(2,fftSize);
-
-  var source, bufferLength, dataArray, barWidth, barHeight;
-  var mainColor = mainColor;
-
   //---------------------------------------------------------
   //generate(): generates frequency chart
   //---------------------------------------------------------
-  this.generate = function(url){
+  this.generate = function() {
+    if (isGenerated)
+      return;
+
+    // Audio element
+    var audio = new Audio();
     audio.src = url;
-    audio.controls = true;
+    audio.controls = useAudioControls;
     document.body.appendChild(audio);
+
+    //audio context
+    audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    analyser = audioCtx.createAnalyser();
+
+    fftSize = Math.pow(2, fftSize);
 
     //audio source
     source = audioCtx.createMediaElementSource(audio);
@@ -216,48 +236,211 @@ function FrequencyChart({
     bufferLength = analyser.frequencyBinCount;
     dataArray = new Uint8Array(bufferLength);
     barWidth = (WIDTH / bufferLength);
+    isGenerated = true;
     this.render();
   }
 
   //---------------------------------------------------------
   //draw():
   //---------------------------------------------------------
-  this.draw = function(){
+  this.draw = function() {
     analyser.getByteFrequencyData(dataArray);
 
     //clear canvas
     canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
     var x = 0;
-    for(var i = 0; i < bufferLength; i++) {
+    for (var i = 0; i < bufferLength; i++) {
       //get barHeight data
       barHeight = dataArray[i];
 
       //draw bars using canvas
       this.drawBars(x, barWidth, barHeight, amplitude, canvasCtx, HEIGHT);
-
       x += barWidth + spacing;
     }
   }
+
   //---------------------------------------------------------
   //setBarStyle(): Used inside the draw function. It is made available
   //               through the object so it can be overriden with custom styles
   //               without having to override the whole draw function
   //---------------------------------------------------------
-  this.drawBars = function(currentX, barWidth, barHeight, amplitude, ctx, ctxHeight){
+  this.drawBars = function(currentX, barWidth, barHeight, amplitude, ctx, ctxHeight) {
     //top bar
     ctx.fillStyle = mainColor;
-    ctx.fillRect(currentX, ctxHeight/2-barHeight * amplitude,barWidth,barHeight * amplitude);
+    ctx.fillRect(currentX, ctxHeight / 2 - barHeight * amplitude, barWidth, barHeight * amplitude);
 
     //bottom bar
     ctx.fillStyle = bottomColor;
-    ctx.fillRect(currentX, Math.floor(ctxHeight/2), barWidth, barHeight * bottomAmplitude);
+    ctx.fillRect(currentX, Math.floor(ctxHeight / 2), barWidth, barHeight * bottomAmplitude);
   }
+
   //---------------------------------------------------------
   //render(): render loop drawing on canvas
   //---------------------------------------------------------
-  this.render = function(){
+  this.render = function() {
     this.draw();
-    requestAnimationFrame(() => {this.render();});
+    requestAnimationFrame(() => {
+      this.render();
+    });
+  }
+}
+
+//==============================================================================
+//FrequencyChart: uses analyser node to create bar chart of current playing audio
+//==============================================================================
+function FrequencyChart({
+  container,
+  url,
+  barSize = 5,
+  amplitude = 0.33,
+  fftSize = 8,
+  spacing = 2,
+  bottomAmplitude = amplitude,
+  mainColor = "#42cef4",
+  bottomColor = mainColor
+}) {
+
+  var audioCtx;
+  var analyser;
+  var source, bufferLength, dataArray, barWidth, barHeight;
+  var mainColor = mainColor;
+  var isGenerated = false;
+
+  //timing variables
+  var playing = false;
+  var playbackTime = 0;
+  var pastTime = 0;
+  var source;
+
+  // Get container for the canvas.
+  var container = document.getElementById(container);
+
+  //audio context
+  audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+  analyser = audioCtx.createAnalyser();
+  var fftSize = Math.pow(2, fftSize);
+  analyser.fftSize = fftSize;
+
+  // Setup canvas. Keeping accessible for user manipulation.
+  this.canvas = document.createElement('canvas');
+  this.canvas.width = container.offsetWidth;
+  this.canvas.height = container.offsetHeight;
+  var canvasCtx = this.canvas.getContext("2d");
+  container.appendChild(this.canvas);
+
+  const WIDTH = this.canvas.width;
+  const HEIGHT = this.canvas.height;
+
+  bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
+  barWidth = (WIDTH / bufferLength);
+
+  //---------------------------------------------------------
+  //generate(): Loads audio and launches animation
+  //               once audio is ready
+  //---------------------------------------------------------
+  this.generate = function(url) {
+    document.addEventListener('audioReady', () => {
+      this.render();
+    }, false);
+    this.loadAudio(url);
+  }
+
+  //---------------------------------------------------------
+  //draw():
+  //---------------------------------------------------------
+  this.draw = function() {
+    analyser.getByteFrequencyData(dataArray);
+
+    //clear canvas
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    var x = 0;
+    for (var i = 0; i < bufferLength; i++) {
+      //get barHeight data
+      barHeight = dataArray[i];
+
+      //draw bars using canvas
+      this.drawBars(x, barWidth, barHeight, amplitude, canvasCtx, HEIGHT);
+      x += barWidth + spacing;
+    }
+  }
+
+  //---------------------------------------------------------
+  //playPause(): alternates between playing and pausing playback
+  //---------------------------------------------------------
+  this.playPause = function() {
+    if (!playing) {
+      source = audioCtx.createBufferSource();
+      source.buffer = this.audBuffer;
+      source.connect(analyser);
+      analyser.connect(audioCtx.destination);
+      source.start(audioCtx.currentTime, playbackTime);
+      pastTime = audioCtx.currentTime;
+      playing = true;
+    }
+    else {
+      source.stop();
+      playing = false;
+    }
+  }
+
+  //---------------------------------------------------------
+  //loadAudio(): loads the audio and stores results
+  //---------------------------------------------------------
+  this.loadAudio = function(url) {
+    var readyEvent = new Event('audioReady');
+    var self = this;
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+
+    // Decode asynchronously
+    request.onload = function() {
+      audioCtx.decodeAudioData(request.response,
+        function(buffer) {
+          self.audBuffer = buffer;
+          self.duration = buffer.duration;
+          document.dispatchEvent(readyEvent);
+        });
+    }
+    request.send();
+  }
+
+  //---------------------------------------------------------
+  //setBarStyle(): Used inside the draw function. It is made available
+  //               through the object so it can be overriden with custom styles
+  //               without having to override the whole draw function
+  //---------------------------------------------------------
+  this.drawBars = function(currentX, barWidth, barHeight, amplitude, ctx, ctxHeight) {
+    //top bar
+    ctx.fillStyle = mainColor;
+    ctx.fillRect(currentX, ctxHeight / 2 - barHeight * amplitude, barWidth, barHeight * amplitude);
+
+    //bottom bar
+    ctx.fillStyle = bottomColor;
+    ctx.fillRect(currentX, Math.floor(ctxHeight / 2), barWidth, barHeight * bottomAmplitude);
+  }
+
+  //---------------------------------------------------------
+  //setPlayback(): Handles progression of playback during render
+  //---------------------------------------------------------
+  function syncTime() {
+    if (playing) {
+      playbackTime = playbackTime + audioCtx.currentTime - pastTime;
+      pastTime = audioCtx.currentTime;
+    }
+  }
+
+  //---------------------------------------------------------
+  //render(): render loop drawing on canvas
+  //---------------------------------------------------------
+  this.render = function() {
+    this.draw();
+    syncTime();
+    requestAnimationFrame(() => {
+      this.render();
+    });
   }
 }
